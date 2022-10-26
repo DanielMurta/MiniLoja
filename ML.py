d9414038a66843c4170280628c2ec6f6af2e26ac
Daniel Silveira <murtdaniel@gmail.com>
2022-10-26 15:48:36 -0300

    Mini Loja 1.0

--- ML.py
+++ ML.py
@@ -18,6 +18,7 @@ while True:
     escolha_menu = int(input('O que deseja comprar? (0 para finalizar compra): '))
     if escolha_menu == 0:
         break
+    lista_de_compras = []
     while True:
         if escolha_menu == 1:
             print('_'*30)
@@ -25,33 +26,34 @@ while True:
             print('[1] ARROZ - R$4,99 -- '
                 '[2] FEIJÃO - R$5,59')
 
-        lista_de_compras = {}
         seleção_de_comidas = int(input('Selecione o produto: '))
         if seleção_de_comidas == 1:
-            quant_de_arroz = int(input('Quantidade de ARROZ: '))
-            lista_de_compras['arroz'] = valor_total_arroz = quant_de_arroz * 4.99
+            lista_de_compras.append(int(input('Quantidade de ARROZ: ')) * 4.99)
         elif seleção_de_comidas == 2:
-            quant_de_feijao = int(input('Quantidade de FEIJÃO: '))
-            lista_de_compras['feijao'] = valor_total_feijao = quant_de_feijao * 5.59
+            lista_de_compras.append(int(input('Quantidade de FEIJÃO: ')) * 5.59)
+
+
 
         r = str(input('Deseja continuar comprando?[S/N]: ')).strip().upper()
         if r == 'N':
             break
 print(lista_de_compras)
-#print('-'*30)
-#print('À vista DINHEIRO/CHEQUE [1]\nÀ vista no cartão       [2]\nEm até 2x no cartão '
-#      '    [3]\n3x ou mais no cartão    [4]')
-#print('-'*30)
-#CP = int(input('Qual a condição de pagamento? '))
-#if CP == 1:
-#    print("Com desconto de 10% o valor será de R${:.2f}".format(Preço - Preço*0.1))
-#elif CP == 2:
-#    print('Com desconto de 5% o valor será de R${:.2f}'.format(Preço - Preço*0.05))
-#elif CP == 3:
-#    print('R${:.2f} parcelado em 2x'.format(Preço))
-#elif CP == 4:
-#    parcela = int(input('Quantas parcelas? :'))
-#    print('Sua compra será parcelada em {}x.'.format(parcela))
-#    print('O total será R${:.2f} com juros de 20%.'.format(Preço + Preço*0.2))
-#else:
-#    print("Opção inválida de pagamento. Tente novamente!")
+valor_total = sum(lista_de_compras)
+print(f'O valor total da sua compra é R${valor_total:.2f}')
+print('-'*30)
+print('À vista DINHEIRO/CHEQUE [1]\nÀ vista no cartão       [2]\nEm até 2x no cartão '
+      '    [3]\n3x ou mais no cartão    [4]')
+print('-'*30)
+CP = int(input('Qual a condição de pagamento? '))
+if CP == 1:
+    print("Com desconto de 10% o valor será de R${:.2f}".format(valor_total - (valor_total*0.1)))
+elif CP == 2:
+    print('Com desconto de 5% o valor será de R${:.2f}'.format(valor_total - (valor_total*0.05)))
+elif CP == 3:
+    print('R${:.2f} parcelado em 2x'.format(valor_total))
+elif CP == 4:
+    parcela = int(input('Nº Parcelas: '))
+    print('Sua compra será parcelada em {}x.'.format(parcela))
+    print('O total será R${:.2f} com juros de 20%.'.format(valor_total + valor_total*0.2))
+else:
+    print("Opção inválida de pagamento. Tente novamente!")
